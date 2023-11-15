1d69c11a8b65302e2cb198854b4200c1c36e3e2a
Scott Kirian-Cressey <scott.kirian-cressey@protonmail.com>
2023-11-15 13:04:45 -0500

    Attemting to re-commit due to lack of understanding xcode integration

--- ivy_reporting.py
+++ ivy_reporting.py
@@ -1,4 +1,8 @@
-# Script for reading Data Lake CSV file from Ivy.ai 
+# Script for reading Data Lake CSV file from Ivy.ai
+# Script assumes .csv file exported from Ivy.ai with following filters:
+# Chat Length, Messages to Bot, Bot Responses (Generative),
+#   Bot Responses (Retrieval), Bot Responses (Low Confidence),
+#   Bot Responses (No Confidence).
 
 #!/usr/bin/env python3      #Run as Unix script
 
@@ -11,11 +15,13 @@ filename = input('Please enter the name of an Ivy Data Lake file:\n')
 
 with open(filename, newline='') as csvfile:
 
-    log = csv.DictReader(csvfile, fieldnames= ("chat_id", "start_time",
-        "start_day", "end_time", "length", "user_messages",
-        "bot_low_conf", "bot_no_conf"))
+    log = csv.DictReader(csvfile, fieldnames= ("chat_id", "length",
+        "user_messages", "bot_gen", "bot_retrieval", "bot_low_conf",
+        "bot_no_conf"))
 	
     total_chats = 0             #total number of unique chats
+    total_gen = 0               #total generative responses
+    total_retrieval = 0         #total retrieval responses
     total_low_conf = 0          #sum of num_low_conf
     total_no_conf = 0           #sum of num_no_conf
     total_user_messages = 0     #sum of user_messages
@@ -25,6 +31,9 @@ with open(filename, newline='') as csvfile:
     next(log)                #skip first row to remove column labels
 
     for chat in log:
+        
+        #DELETE ME
+        print(chat["bot_retrieval"])
         
         #do not include data for chats with no length
         if not chat["length"]:
@@ -41,6 +50,12 @@ with open(filename, newline='') as csvfile:
         
         if chat["user_messages"]:
             total_user_messages += int(chat["user_messages"])
+        
+        if chat["bot_gen"]:
+            total_gen += int(chat["bot_gen"])
+            
+        if chat["bot_retrieval"]:
+            total_retrieval += int(chat["bot_retrieval"])
             
         if chat["bot_low_conf"]:
             total_low_conf += int(chat["bot_low_conf"])
@@ -59,5 +74,7 @@ with open(filename, newline='') as csvfile:
     print("Total low confidence responses: ", total_low_conf)
     print("Total no confidence responses: ", total_no_conf)
     print("Total low/no confidence responses: ", total_low_conf + total_no_conf)
+    print("Generative responses + retrieval responses: ",
+        total_gen + total_retrieval)
     print("High confidence response rate: ",
         (total_high_conf / total_chats)*100, "%")
--- read_me.txt
+++ read_me.txt
@@ -1,2 +1,14 @@
-#  <#Title#>
+Read Me: ivy_reporting.py
+
+Script assumes .csv file exported from Ivy.ai with following filters:
+Chat Length, Messages to Bot, Bot Responses (Generative),
+Bot Responses (Retrieval), Bot Responses (Low Confidence),
+Bot Responses (No Confidence)
+
+Note that Data Lake exports prior to November 2023 will not include reporting
+of generative responses or retrieval responses. Instead,
+TOTAL [total messages or total chats? TBD] - low confidence - no confidence may
+be used as a proxy. 
+
+
 
