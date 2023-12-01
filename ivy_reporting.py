d7a7f8465b5cdd4ef7665832adc7cc6a2480b007
Scott Kirian-Cressey <scott.kirian-cressey@protonmail.com>
2023-12-01 16:52:59 -0500

    Converted to pass around a report object

--- ivy_reporting.py
+++ ivy_reporting.py
@@ -8,89 +8,106 @@
 
 import csv
 
-total_chats = 0             #total number of unique chats
-total_gen = 0               #total generative responses
-total_retrieval = 0         #total retrieval responses
-total_low_conf = 0          #sum of num_low_conf
-total_no_conf = 0           #sum of num_no_conf
-total_user_messages = 0     #sum of user_messages
-zero_time_chats = 0         #filter out non-real chats based on time
-zero_message_chats = 0      #filter out non-real chats based on interaction
-valid_filename = False      #loop condition to get valid filename
+class Report:
+    
+    total_chats = 0             #total number of unique chats
+    total_gen = 0               #total generative responses
+    total_retrieval = 0         #total retrieval responses
+    total_high_conf = 0
+    total_low_conf = 0          #sum of num_low_conf
+    total_no_conf = 0           #sum of num_no_conf
+    total_user_messages = 0     #sum of user_messages
+    zero_time_chats = 0         #filter out non-real chats based on time
+    zero_message_chats = 0      #filter out non-real chats based on interaction
+    
+def read_report():
 
-while valid_filename == False:
+    valid_filename = False      #loop condition to get valid filename
+    report = Report()
 
-    try:
-        filename = input('Please enter the name of an Ivy Data Lake file:\n')
-        
-        if filename == 'quit' or filename == 'q' or filename == 'Quit':
-            break
+    while valid_filename == False:
 
-        #Fragile: Ensure CSV contains only fields enumerated for fieldnames
-        #Else, garbage in garbage out.
-        with open(filename, newline='') as csvfile:
-        
+        try:
+            filename = input('Please enter the name of an Ivy Data Lake file:\n')
+            
+            if filename == 'quit' or filename == 'q' or filename == 'Quit':
+                break
+
+            #Fragile: Ensure CSV contains only fields enumerated for fieldnames
+            #Else, garbage in garbage out.
+            csvfile = open(filename, newline='')
+            #FIXME DELETE
+            print("Opened file")
+            
             log = csv.DictReader(csvfile, fieldnames= ("chat_id", "length",
-                "user_messages", "bot_gen", "bot_retrieval", "bot_low_conf",
-                "bot_no_conf"))
-	
+                    "user_messages", "bot_gen", "bot_retrieval", "bot_low_conf",
+                    "bot_no_conf"))
+        
             valid_filename = True
             next(log)                #skip first row containing column labels
 
             #parse file data
             for chat in log:
-                
+                    
                 #FIXME commented out for clarity at the moment:
                 #do not include data for chats with no length
                 #if not chat["length"]:
                 #    zero_time_chats += 1
                 #    continue
-            
+                    
                 #FIXME commented out for clarity at the moment:
                 #do not include data for chats with no message from the user
-                if not chat["user_messages"]:
-                    zero_message_chats += 1
-                    continue
-            
+                #    if not chat["user_messages"]:
+                #        reports.zero_message_chats += 1
+                #        continue
+                
 
                 #do not count chats filtered from above against total
-                total_chats += 1
+                report.total_chats += 1
         
                 if chat["user_messages"]:
-                    total_user_messages += int(chat["user_messages"])
+                    report.total_user_messages += int(chat["user_messages"])
         
                 if chat["bot_gen"]:
-                    total_gen += int(chat["bot_gen"])
+                    report.total_gen += int(chat["bot_gen"])
             
                 if chat["bot_retrieval"]:
-                    total_retrieval += int(chat["bot_retrieval"])
+                    report.total_retrieval += int(chat["bot_retrieval"])
             
                 if chat["bot_low_conf"]:
-                    total_low_conf += int(chat["bot_low_conf"])
+                    report.total_low_conf += int(chat["bot_low_conf"])
         
                 if chat["bot_no_conf"]:
-                    total_no_conf += int(chat["bot_no_conf"])
-           
-    except:
-        print("\nInvalid filename.")
-        print("Enter filename or type 'quit'.\n")
-        continue
+                    report.total_no_conf += int(chat["bot_no_conf"])
+        
+            csvfile.close()
+            
+            #FIXME Move this under the printer function. Keep reader strictly read
+            #report.total_high_conf = report.total_gen + report.total_retrieval
 
-if valid_filename == False:
-    raise SystemExit(0)
+            
+        except:
+            print("\nInvalid filename.")
+            print("Enter filename or type 'quit'.\n")
+            continue
 
-total_high_conf = total_gen + total_retrieval
+    if valid_filename == False:
+        raise SystemExit(0)
+        
+    return report
+
+report = read_report()
 
 #print("Filtered chats with zero seconds: ", zero_time_chats)
 #print("Filtered chats with no user interaction: ", zero_message_chats)
-print("Total unique chats served: ", total_chats)
-print("Total messages from users: ", total_user_messages)
-print("Total generative responses: ", total_gen)
-print("Total retrieval responses: ", total_retrieval)
-print("Total low confidence responses: ", total_low_conf)
-print("Total no confidence responses: ", total_no_conf)
-print("Total high confidence responses: ", total_high_conf)
+print("Total unique chats served: ", report.total_chats)
+print("Total messages from users: ", report.total_user_messages)
+print("Total generative responses: ", report.total_gen)
+print("Total retrieval responses: ", report.total_retrieval)
+print("Total low confidence responses: ", report.total_low_conf)
+print("Total no confidence responses: ", report.total_no_conf)
+print("Total high confidence responses: ", report.total_high_conf)
       
-if total_chats:                                #avoid div by 0
+if report.total_chats:                                #avoid div by 0
     print("High confidence response rate: ",
-        (total_high_conf / total_chats)*100, "%")
+        (report.total_high_conf / report.total_chats)*100, "%")
